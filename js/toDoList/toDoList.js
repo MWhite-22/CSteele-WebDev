7522d95945a851a354fc65a159de630585ae315c
Michael White <mjw2291@gmail.com>
2018-10-08 14:00:41 -0400

    Added the ToDoList project

--- js/toDoList/toDoList.js
+++ js/toDoList/toDoList.js
@@ -6,13 +6,35 @@ var input = prompt("What would you like to do?").toUpperCase();
 
 while(input !== "QUIT"){
 	if (input === "NEW"){
-		var newToDo = prompt("What do you need to add to the ToDo list?").toUpperCase();
-		toDo.push(newToDo);
+		newToDo();
 	}	else if (input === "LIST") {
-		alert("Your current ToDo list is: " + toDo);
+		listToDo();
+	} else if (input === "DELETE") {
+		deleteToDo();
 	}
 	
 	input = prompt("What would you like to do?").toUpperCase();
 }
 
 alert("Goodbye");
+
+//*****FUNCTION DECLARATIONS*****
+function newToDo(){
+	var newToDo = prompt("What do you need to add to the ToDo list?").toUpperCase();
+	toDo.push(newToDo);
+	console.log(newToDo + " has been added to your ToDo list");
+}
+
+function listToDo(){
+	console.log("***** TODO LIST *****");
+	toDo.forEach(function(item, index){
+		console.log(index + ": " + item);
+	});
+	console.log("***************");
+}
+
+function deleteToDo(){
+	var deleteIndex = prompt("Enter the index of the Item you wish to delete");
+	toDo.splice(deleteIndex, 1);
+	console.log("Item " + deleteIndex + " deleted");
+}
