345076acd986fd6a60d937981c0a7f28a38c3cfc
Michael White <mjw2291@gmail.com>
2018-10-08 22:37:17 -0400

    Updated ScoreKeeper Project

--- js/scoreKeeper/score.js
+++ js/scoreKeeper/score.js
@@ -2,8 +2,7 @@ document.addEventListener("DOMContentLoaded", function(){
     button1.addEventListener("click", addPoint1);
     button2.addEventListener("click", addPoint2);
     reset.addEventListener("click", resetScore);
-
-    playToInput.addEventListener("change", newPlayTo);
+    playToInput.addEventListener("input", newPlayTo);
 });
 
 var p1display = document.querySelector("#p1display");
@@ -14,23 +13,32 @@ var button1 = document.querySelector("button#player1");
 var button2 = document.querySelector("button#player2");
 var reset = document.querySelector("#reset");
 
+var gameOver = false;
 var p1score = 0;
 var p2score = 0;
 var playTo = 5;
 
 function addPoint1(){
+    if (gameOver){
+        return;
+    }
     p1score++;
     p1display.innerHTML = p1score;
     if (p1score == playTo){
         p1display.classList.add("winner");
+        gameOver = true;
     }
 }
 
 function addPoint2(){
+    if (gameOver){
+        return;
+    }
     p2score++;
     p2display.innerHTML = p2score;
     if (p2score == playTo){
         p2display.classList.add("winner");
+        gameOver = true;
     }
 }
 
@@ -39,6 +47,9 @@ function resetScore(){
     p2score = 0
     p1display.innerHTML = p1score;
     p2display.innerHTML = p2score;
+    p1display.classList.remove("winner");
+    p2display.classList.remove("winner");
+    gameOver = false;
 }
 
 function newPlayTo(){
