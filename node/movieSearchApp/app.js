eac556c1f337845ea378de6d3a501c387352501b
Michael White <mjw2291@gmail.com>
2018-10-18 18:50:57 -0400

    implemented API call to OMDB & iterated over the result names

--- node/movieSearchApp/app.js
+++ node/movieSearchApp/app.js
@@ -15,12 +15,43 @@ app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
 // ========================================
+// 			Variable Declaration
+// ========================================
+
+function omdbQuery(e){
+	return("http://www.omdbapi.com/?apikey=thewdb&s=" + e);
+
+}
+
+// ========================================
 // 			Routes
 // ========================================
 
 app.get('/', (req, res)=>{
 	res.render("home");
-})
+});
+
+app.get('/movies/:search', (req, res)=>{
+	const search = req.params.search;
+	let query = omdbQuery(search);
+	request(query, (error, response, body)=>{
+		let parsedBody = JSON.parse(body);
+		if(!error && response.statusCode == 200){
+			res.render("movieList", {search: search, body: parsedBody});
+		} else {
+			res.render("movieList", {search: search, body: "Fail"});
+		}
+	});
+});
+
+app.get('/movie/:id', (req, res)=>{
+	const id = req.params.id;
+	res.render("movie", {id: id});
+});
+
+app.get('*', (req, res)=>{
+	res.send("YOU TOOK A WRONG TURN");
+});
 
 // ========================================
 // 			Listen
@@ -28,4 +59,4 @@ app.get('/', (req, res)=>{
 
 app.listen(port, ()=>{
 	console.log('MovieDB Started on port ' + port);
-})
+});
