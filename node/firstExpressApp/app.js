d8ce99ecdd6b1908a44e9d52526e3161a26278a5
Michael White <mjw2291@gmail.com>
2018-10-18 14:11:32 -0400

    Added public static folders, partial views, include header/footer

--- node/firstExpressApp/app.js
+++ node/firstExpressApp/app.js
@@ -1,9 +1,24 @@
+// ========================================
+// 			Imports/Requires
+// ========================================
+
 const express = require("express");
 const app = express();
 const port = "3000";
 
+// ========================================
+// 			Express Configure
+// ========================================
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+// ========================================
+// 			Routes
+// ========================================
+
 app.get("/", function(req, res){
-	res.render("home.ejs");
+	res.render("home");
 });
 
 app.get("/posts", function(req, res){
@@ -12,17 +27,21 @@ app.get("/posts", function(req, res){
 		{title: "Post 2", author: "Bobby"},
 		{title: "Post 3", author: "Susy Q"}
 	];
-	res.render("posts.ejs", {posts: posts});
+	res.render("posts", {posts: posts});
 });
 
 app.get("/:thing", function(req, res){
 	let thing = req.params.thing;
-	res.render("example.ejs", {thing: thing});
+	res.render("example", {thing: thing});
 });
 
 app.get("*", function(req, res){
 	res.send("PAGE NOT FOUND");
 });
+
+// ========================================
+// 			Listen Function
+// ========================================
 
 app.listen(port, function(){
 	console.log("Server started on port " + port);
