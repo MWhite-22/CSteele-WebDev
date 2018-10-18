8f68f08d8015b8011c4405b16b54bb50cc7ec85d
Michael White <mjw2291@gmail.com>
2018-10-18 17:31:45 -0400

    Updated apiTest to use JSON parsing

--- node/apiTest/app.js
+++ node/apiTest/app.js
@@ -1,11 +1,10 @@
-let request = require("request");
-request('http://www.google.com', function(err, res, body){
-	if(err){
-		console.log("ERROR: Something went wrong");
-		console.log(err);
-	} else {
-		if (res.statusCode == 200){
-			console.log(body);
-		}
+const request = require("request");
+
+const url = "https://query.yahooapis.com/v1/public/yql?q=select%20astronomy.sunset%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22maui%2C%20hi%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
+
+request(url, function(err, res, body){
+	if(!err && res.statusCode == 200){
+		let parsedBody = JSON.parse(body);
+		console.log(parsedBody.query.results.channel.astronomy.sunset);
 	}
 });
