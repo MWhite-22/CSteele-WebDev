070928e86b5033c193fe175e2315260e1058947a
Michael White <mjw2291@gmail.com>
2018-10-22 15:22:45 -0400

    Added Mongoose for Data Persistence. All personal data stored in db.js

--- YelpCamp/server.js
+++ YelpCamp/server.js
@@ -4,8 +4,24 @@
 const express = require ('express');
 const app = express();
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const port = "8080";
+
+// ========================================
+// 			DB Connect
+// ========================================
+const db = require('./db');
+
+mongoose.connect(db, {useNewUrlParser: true});
+
+// Setting Schema
+let campSchema = new mongoose.Schema({
+	name: {type: String, default: 'Default Name'},
+	image: String
+});
+
+let Campground = mongoose.model('Campground', campSchema);
 
 // ========================================
 // 			Node Config
@@ -15,15 +31,6 @@ app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: true}));
 
 // ========================================
-// 			Temp Vars
-// ========================================
-let campgrounds = [
-	{name: 'Salmon Creek', image: 'https://images.pexels.com/photos/450441/pexels-photo-450441.jpeg?cs=srgb&dl=adventure-boy-camping-450441.jpg&fm=jpg'},
-	{name: 'Hilly Hill', image: 'https://images.pexels.com/photos/1061640/pexels-photo-1061640.jpeg?cs=srgb&dl=bonfire-camp-campfire-1061640.jpg&fm=jpg'},
-	{name: 'Mnt. Mountain', image: 'https://images.pexels.com/photos/803226/pexels-photo-803226.jpeg?cs=srgb&dl=camping-clouds-dawn-803226.jpg&fm=jpg'},
-];
-
-// ========================================
 // 			Routes
 // ========================================
 app.get('/', (req, res)=>{
@@ -31,15 +38,28 @@ app.get('/', (req, res)=>{
 });
 
 app.get('/campgrounds', (req, res)=>{
-	res.render('campgrounds', {campgrounds: campgrounds});
+	Campground.find({}, (err, campgrounds)=>{
+		if(err){
+			console.log('Error:');
+			console.log(err);
+		} else {
+			res.render('campgrounds', {campgrounds: campgrounds});
+		}
+	})
 });
 
 app.post('/campgrounds', (req, res)=>{
 	let name = req.body.name;
 	let imgURL = req.body.imgURL;
 	let newCampground = {name: name, image: imgURL};
-	campgrounds.push(newCampground);
-	res.redirect('/campgrounds');
+	Campground.create(newCampground, (err, cg)=>{
+		if(err){
+			console.log('Error:');
+			console.log(err);
+		} else {
+			res.redirect('/campgrounds');
+		}
+	});
 });
 
 app.get('/campgrounds/new', (req, res)=>{
