114c86df0ce70eecadcb5455953e0ece5e84d987
Michael White <mjw2291@gmail.com>
2018-10-22 13:51:05 -0400

    added Mongoose for MongoDB integration

--- YelpCamp/server.js
+++ YelpCamp/server.js
@@ -15,6 +15,15 @@ app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: true}));
 
 // ========================================
+// 			Temp Vars
+// ========================================
+let campgrounds = [
+	{name: 'Salmon Creek', image: 'https://images.pexels.com/photos/450441/pexels-photo-450441.jpeg?cs=srgb&dl=adventure-boy-camping-450441.jpg&fm=jpg'},
+	{name: 'Hilly Hill', image: 'https://images.pexels.com/photos/1061640/pexels-photo-1061640.jpeg?cs=srgb&dl=bonfire-camp-campfire-1061640.jpg&fm=jpg'},
+	{name: 'Mnt. Mountain', image: 'https://images.pexels.com/photos/803226/pexels-photo-803226.jpeg?cs=srgb&dl=camping-clouds-dawn-803226.jpg&fm=jpg'},
+];
+
+// ========================================
 // 			Routes
 // ========================================
 app.get('/', (req, res)=>{
@@ -22,18 +31,15 @@ app.get('/', (req, res)=>{
 });
 
 app.get('/campgrounds', (req, res)=>{
-	let campgrounds = [
-		{name: 'Salmon Creek', image: 'https://images.pexels.com/photos/450441/pexels-photo-450441.jpeg?cs=srgb&dl=adventure-boy-camping-450441.jpg&fm=jpg'},
-		{name: 'Hilly Hill', image: 'https://images.pexels.com/photos/1061640/pexels-photo-1061640.jpeg?cs=srgb&dl=bonfire-camp-campfire-1061640.jpg&fm=jpg'},
-		{name: 'Mnt. Mountain', image: 'https://images.pexels.com/photos/803226/pexels-photo-803226.jpeg?cs=srgb&dl=camping-clouds-dawn-803226.jpg&fm=jpg'},
-	];
 	res.render('campgrounds', {campgrounds: campgrounds});
 });
 
 app.post('/campgrounds', (req, res)=>{
-	let name = req.params.name;
-	let imgURL = req.params.imgURL;
-	res.send('POST ROUTE for CAMPGROUNDS');
+	let name = req.body.name;
+	let imgURL = req.body.imgURL;
+	let newCampground = {name: name, image: imgURL};
+	campgrounds.push(newCampground);
+	res.redirect('/campgrounds');
 });
 
 app.get('/campgrounds/new', (req, res)=>{
