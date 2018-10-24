a0dde6823b45ed9d09cca27d1dbfe91ceccade0f
Michael White <mjw2291@gmail.com>
2018-10-24 15:47:47 -0400

    Added to Campground Schema Model

--- YelpCamp/models/campground.js
+++ YelpCamp/models/campground.js
@@ -4,7 +4,9 @@ let campSchema = new mongoose.Schema({
 	name: {type: String, default: 'Default Name'},
 	image: String,
 	description: String,
-	likes: {type: Number, default: 0}
+	likes: {type: Number, default: 0},
+	score: Number,
+	created: {type: Date, defaut: Date.now}
 });
 
 module.exports = mongoose.model('Campground', campSchema);
