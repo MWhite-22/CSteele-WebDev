cbadc0bd6e47b1b602990f0ebcff160f4325ed05
Michael White <mjw2291@gmail.com>
2019-02-02 19:36:39 -0500

    Added User model. Update index.js

--- node/authApp/index.js
+++ node/authApp/index.js
@@ -1,12 +1,38 @@
-const   bodyParser 		= require('body-parser'),
-        mongoose        = require('mongoose'),
-        express         = require('express'),
-        app             = express(),
-        port            = '8080'
+const   bodyParser 		        = require('body-parser'),
+        passport                = require('passport'),
+        LocalStrategy           = require('passport-local'),
+        passportLocalMongoose   = require('passport-local-mongoose'),
+        mongoose                = require('mongoose'),
+        expressSession          = require('express-session'),
+        User 			        = require('./models/user');
+        express                 = require('express'),
+        app                     = express(),
+        db 				        = require('./db');
+        port                    = '8080'
 
+//Open DB Connection
+mongoose.connect(db, {useNewUrlParser: true}, (err)=>{
+	if(err){
+		console.log('DB CONNECTION ERROR:');
+		console.log(err);
+	} else {
+		console.log('Successful Connection to Database: authApp');
+	}
+});
+
+//Config express 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(expressSession({
+    secret: 'Secrets secrets are no fun',
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
+
+//Routes
 app.get('/', (req, res)=>{
     res.render('index');
 });
@@ -19,6 +45,8 @@ app.get('/*', (req, res)=>{
     res.render('index');
 });
 
+
+//Listen
 app.listen(port, ()=>{
 	console.log(`Server started on port: ${port}`);
 });
