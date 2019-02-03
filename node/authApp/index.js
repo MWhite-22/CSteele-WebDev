d4bd9894fec6c51e9fd0a337ac55e7a4834fe200
Michael White <mjw2291@gmail.com>
2019-02-03 12:16:43 -0500

    Finished authApp with login middleware and logout functionality

--- node/authApp/index.js
+++ node/authApp/index.js
@@ -31,14 +31,67 @@ app.use(expressSession({
 app.use(passport.initialize());
 app.use(passport.session());
 
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+// ========================================
+// 			Middleware
+// ========================================
+function isLoggedIn(req,res,next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect('/login');
+}
+
 
 //Routes
 app.get('/', (req, res)=>{
     res.render('index');
 });
 
-app.get('/secret', (req, res)=>{
+app.get('/secret', isLoggedIn, (req, res)=>{
     res.render('secret');
+});
+
+//Login Routes
+app.get('/login', (req, res)=>{
+    res.render('login');
+});
+
+app.post('/login', passport.authenticate("local",
+    {
+        successRedirect: '/secret',
+        failureRedirect: '/login'
+    }), (req, res)=>{
+        res.redirect('/secret');
+});
+
+app.get('/logout', (req, res)=>{
+    req.logout();
+    res.redirect('/');
+});
+
+
+//Auth Routes
+app.get('/register', (req, res)=>{
+    res.render('register');
+});
+
+app.post('/register', (req, res)=>{
+    const username = req.body.username;
+    const password = req.body.password;
+    User.register(new User({username: username}),password,(err, user)=>{
+        if(err){
+            console.log(err);
+            res.redirect('/register');
+        } else {
+            passport.authenticate("local")(req, res, ()=>{
+                res.redirect('/secret');
+            });
+        }
+    })
 });
 
 app.get('/*', (req, res)=>{
